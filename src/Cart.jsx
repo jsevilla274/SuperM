cde825401f4df2c60fa2cd51fef3247a483bfc5b
jsevilla274 <jsevilla274@gmail.com>
2023-11-26 18:56:20 -0500

    Initial commit

---
+++ src/Cart.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
+
+
+export default function Cart({ cart }) {
+  const totalPrice = cart.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+
+  const [email, setEmail] = useState("");
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    // NOTE: This is the area where you would perform some form of ajax call/redirect to the payment service
+    alert('communicated with backend to complete purchase!');
+  }
+
+  return (
+    <div className="cart-layout">
+      <div>
+        <h1>Your Cart</h1>
+        {cart.length === 0 && (
+          <p>You have not added any product to your cart yet.</p>
+        )}
+        {cart.length > 0 && (
+          <>
+            <table className="table table-cart">
+              <thead>
+                <tr>
+                  <th width="25%" className="th-product">
+                    Product
+                  </th>
+                  <th width="20%">Unit price</th>
+                  <th width="10%">Quanity</th>
+                  <th width="25%">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((product) => {
+                  return (
+                    <tr key={product.id}>
+                      <td>
+                        <img
+                          src={product.image}
+                          width="30"
+                          height="30"
+                          alt=""
+                        />{" "}
+                        {product.name}
+                      </td>
+                      <td>${product.price}</td>
+                      <td>{product.quantity}</td>
+                      <td>
+                        <strong>${product.price * product.quantity}</strong>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th colSpan="2"></th>
+                  <th className="cart-highlight">Total</th>
+                  <th className="cart-highlight">${totalPrice}</th>
+                </tr>
+              </tfoot>
+            </table>
+            <form className="pay-form" onSubmit={handleFormSubmit}>
+              <p>
+                Enter your email and then click on pay and your products will be
+                delivered to you on the same day!
+                <br />
+                <em>
+                  Enter your own Stripe Publishable Key in Cart.js for the
+                  checkout to work
+                </em>
+              </p>
+              <Input
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+                type="email"
+                required
+              />
+              <Button type="submit">Pay</Button>
+            </form>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
